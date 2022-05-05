9e82142d25ae2b4a26986500ceb89c40e13736bc
Hannan Satopay <sathannan@hotmail.com>
2022-05-05 09:33:46 +0530

    Session 3

---
+++ Session 3/ex4.go
@@ -0,0 +1,18 @@
+package main
+import (
+  "fmt"
+  "time"
+)
+
+func main() {
+  ch1 := make(chan int)
+  go pump(ch1) // pump hangs
+  fmt.Println(<-ch1) // prints only 0
+  time.Sleep(1e9)
+}
+
+func pump(ch chan int) {
+  for i:= 0; ; i++ {
+  ch <- i
+  }
+}
